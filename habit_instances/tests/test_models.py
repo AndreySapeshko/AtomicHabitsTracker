62460be6af41eaa477390951c8b5388f99e8e4a4
Andrey Sapeshko <andrdd17@gmail.com>
2025-12-03 17:32:30 +0300

    Реализована структура тестов. Тесты моделей Habit и HabitInstance, тесты API Habit

---
+++ habit_instances/tests/test_models.py
@@ -0,0 +1,27 @@
+import pytest
+
+from habit_instances.models import HabitInstance, HabitInstanceStatus
+
+from .factory import HabitInstanceFactory
+
+
+@pytest.mark.django_db
+def test_habit_instance_mark_completed():
+    inst = HabitInstanceFactory()
+
+    inst.mark_completed()
+
+    assert inst.status in [
+        HabitInstanceStatus.COMPLETED,
+        HabitInstanceStatus.COMPLETED_LATE,
+    ]
+    assert inst.completed_at is not None
+
+
+@pytest.mark.django_db
+def test_habit_instance_mark_failed():
+    inst = HabitInstanceFactory()
+
+    inst.mark_failed()
+
+    assert inst.status in [HabitInstanceStatus.MISSED, HabitInstanceStatus.FIX_EXPIRED]
