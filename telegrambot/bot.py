1f90f740021c138ab2ea912f93b148ed029f2717
Andrey Sapeshko <andrdd17@gmail.com>
2025-12-07 12:59:59 +0300

    Внесены исправления на соответствие PEP8

--- telegrambot/bot.py
+++ telegrambot/bot.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 logger = logging.getLogger("telegrambot")
 
+
 def get_bot():
     if not settings.TELEGRAM_BOT_TOKEN:
         logger.warning("⚠️ TELEGRAM_BOT_TOKEN is not set — bot is disabled")
