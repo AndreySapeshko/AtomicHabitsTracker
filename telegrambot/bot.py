ac73ab8becf57cf7cd2468d1936aa14ea9beef29
Andrey Sapeshko <andrdd17@gmail.com>
2025-12-07 16:11:21 +0300

    Изменен ci.yml

--- telegrambot/bot.py
+++ telegrambot/bot.py
@@ -10,5 +10,5 @@ def get_bot():
     if not settings.TELEGRAM_BOT_TOKEN:
         logger.warning("⚠️ TELEGRAM_BOT_TOKEN is not set — bot is disabled")
         return None
-
+    print(f"TELEGRAM_BOT_TOKEN: {settings.TELEGRAM_BOT_TOKEN}")
     return Bot(settings.TELEGRAM_BOT_TOKEN)
